d08af1b567a0bd361e17bcd9fdaadf4d2c365da4
Felipe Lima <felipe@ferreiradelima.com>
2015-05-26 12:34:22 -0500

    improved generators

--- chicago_bus_1.js
+++ chicago_bus_1.js
@@ -3,91 +3,95 @@
 "use strict";
 var bus_bucket = bucket("556391a7791aa76a07f0c283"); // Chicago - Bus #1
 
+function random_true_false() {
+    return (_.random(true, false) ? true : false);
+}
+
 const LOCATIONS = [{
     "id": 1,
-    "location": ("41.87" + _.random(7000, 9000) + ", -87.624205"),
+    "location": `41.87${_.random(7000, 9000)}, -87.624205`,
     "max_speed": 45,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 2,
-    "location": "41.88" + _.random(1000, 4000) + ", -87.624205",
+    "location": `41.88${_.random(1000, 4000)}, -87.624205`,
     "max_speed": 0,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 3,
-    "location": "41.88" + _.random(4000, 8000) + ", -87.624448",
+    "location": `41.88${_.random(4000, 8000)}, -87.624448`,
     "max_speed": 20,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 4,
     "location": "41.888330, -87.624792",
     "max_speed": 3,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 5,
     "location": "41.888286, -87.625221",
     "max_speed": 10,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 6,
     "location": "41.887392, -87.626122",
     "max_speed": 8,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 7,
     "location": "41.886912, -87.628783",
     "max_speed": 23,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 8,
     "location": "41.886912, -87.635091",
     "max_speed": 25,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 9,
     "location": "41.885986, -87.636679",
     "max_speed": 3,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 10,
     "location": "41.882344, -87.636936",
     "max_speed": 40,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 11,
     "location": "41.877838, -87.636679",
     "max_speed": 40,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 12,
     "location": "41.875538, -87.635606",
     "max_speed": 50,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 13,
     "location": "41.875569, -87.630842",
     "max_speed": 50,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 14,
     "location": "41.875697, -87.628997",
     "max_speed": 10,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 15,
     "location": "41.875693, -87.626132",
     "max_speed": 5,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 16,
     "location": "41.876340, -87.624190",
     "max_speed": 30,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }, {
     "id": 17,
     "location": "41.877882, -87.624212",
     "max_speed": 10,
-    "break": (_.random(true, false) ? true : false)
+    "break": random_true_false()
 }];
 
 function get_serie(cb) {
@@ -136,7 +140,6 @@ async.parallel({'serie': get_serie, 'id': get_inc_id}, function (err, result) {
     if (err) {
         return console.log(err);
     }
-    console.log(result);
 
     let bus = _.findWhere(LOCATIONS, {"id": result.id});
 
